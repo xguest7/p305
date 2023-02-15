112c2868cf50c0c1fbe1d0ff5a1bdb9adc0aff30
kim sohee <okoksohee@naver.com>
2023-02-15 13:37:14 +0900

    뽑히는 숫자 중복 제거+무한루프 제거
    뽑히는 숫자보다 카운트가 클시, 발생되는 무한루프 제거함
    하지만 인덱스에서 자체적으로 추가로 그럴시에 다시 숫자입력을 받아야함

--- team9/src/team9/dServlet.java
+++ team9/src/team9/dServlet.java
@@ -28,16 +28,22 @@ public class dServlet extends HttpServlet {
 //		int com=(int)(Math.random()*(num2-num1+1)+num1);
 //		rnum.add(com);
 //	}		
-		for(int i=0;i<count;i++) {
-			int com=(int)(Math.random()*(num2-num1+1)+num1);
-			rnum.add(i,com);
-			for(int j=0;j<i;j++) {
-				if(rnum.get(i)==rnum.get(j)) {
-					rnum.remove(i);
-					i--;
+		
+		if((num2-num1+1)>=count) {
+			for(int i=0;i<count;i++) {
+				int com=(int)(Math.random()*(num2-num1+1)+num1);
+				rnum.add(i,com);
+				for(int j=0;j<i;j++) {
+					if(rnum.get(i)==rnum.get(j)) {
+						rnum.remove(i);
+						i--;
+					}
 				}
 			}
-		}
+		}else {
+			System.out.println("범위보다, 카운트 값이 큽니다. \n범위나 카운트를 다시 설정해주세요.");
+		}		
+		
 
 		for(int num:rnum) {
 			System.out.println(num);
