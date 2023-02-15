78ddce4a0b4c2148d93dbfcfac9738bcd8ace619
min soojin <minsj0728@naver.com>
2023-02-15 11:45:50 +0900

    b.jsp 서블렛

---
+++ team9/src/team9/BServlet.java
@@ -0,0 +1,29 @@
+package team9;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/b.jsp")
+public class BServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int num1 = Integer.parseInt(request.getParameter("num1"));
+		int num2 = Integer.parseInt(request.getParameter("num2"));
+		int sum = 0;
+		for (; num1 <= num2; num1++) {
+			sum += num1;
+		}
+		response.getWriter().append(sum+"");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
